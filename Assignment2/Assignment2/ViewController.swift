de9420597e79791a19f4b1bcf6c5b752eb980df3
AKI <a.okada3@gmail.com>
2017-09-20 12:09:26 -0700

    up

---
+++ Assignment2/Assignment2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by 岡田暁 on 2017-09-19.
+//  Copyright © 2017 岡田暁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
