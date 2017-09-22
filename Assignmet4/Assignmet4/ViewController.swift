fc532ebd36b88383cf4ebc686b9cadcaa21bc2af
AKI <a.okada3@gmail.com>
2017-09-21 23:59:58 -0700

    done

---
+++ Assignmet4/Assignmet4/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Assignmet4
+//
+//  Created by 岡田暁 on 2017-09-21.
+//  Copyright © 2017 岡田暁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+//        
+//        self.navigationItem.hidesBackButton = true
+//        let newBackButton = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.plain, target: self, action: #selector(.back(sender:)))
+//        self.navigationItem.leftBarButtonItem = newBackButton
+    }
+
+    
+
+//    func back(sender: UIBarButtonItem) {
+//        // Perform your custom actions
+//        // ...
+//        // Go back to the previous ViewController
+//        _ = navigationController?.popViewController(animated: true)
+//    }
+//  
+
+}
+
