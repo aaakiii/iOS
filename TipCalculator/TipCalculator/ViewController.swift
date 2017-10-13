936545c6ba4f801fa2c5bfcb40aef2825401535e
aaakiii <a.okada3@gmail.com>
2017-10-13 10:46:21 -0700

    add kryboard show&hide

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -8,24 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var billAmountTextField: UITextField!
     @IBOutlet var tipAmountLabel: UILabel!
     @IBOutlet var adjustTipPercentage: UISlider!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        billAmountTextField.delegate = self
         
         // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(self, selector:#selector(ViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         
         adjustTipPercentage.addTarget(self, action: #selector(ViewController.onChangeValueMySlider(sender:)), for: UIControlEvents.valueChanged)
         
+        
     }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        billAmountTextField.resignFirstResponder()
+        return true
 
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+   @objc func keyboardWillShow(notification: Notification){
+    
+    if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+        print("Show")
+        if self.view.frame.origin.y == 0{
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.frame.origin.y -= keyboardSize.size.height
+            })
+        }
+    }
+
+    }
+    @objc func keyboardWillHide(notification: Notification){
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            print("Hide")
+            if self.view.frame.origin.y != 0{
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.view.frame.origin.y += keyboardSize.height
+                })
+            }
+        }
     }
     
     @objc internal func onChangeValueMySlider(sender: UISlider){
