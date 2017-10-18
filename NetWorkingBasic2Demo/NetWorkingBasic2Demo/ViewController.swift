08fac172df1ec890fb11baef1b4e038613cff980
aaakiii <a.okada3@gmail.com>
2017-10-18 12:21:00 -0700

    add row height

---
+++ NetWorkingBasic2Demo/NetWorkingBasic2Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetWorkingBasic2Demo
+//
+//  Created by 岡田暁 on 2017-10-18.
+//  Copyright © 2017 Aki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- SimpleToDoApp/SimpleToDoApp/ToDoListViewController.swift
+++ SimpleToDoApp/SimpleToDoApp/ToDoListViewController.swift
@@ -7,6 +7,8 @@ class ToDoListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         navigationItem.title = "To Do List"
         do{
             let realm = try Realm()
@@ -26,6 +28,10 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        
         return todoItem.count
+    }
+    // Set row height
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
