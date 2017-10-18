87f5447cf5c160958e9668fc35fb6f789a7aa3e3
aaakiii <a.okada3@gmail.com>
2017-10-18 11:21:21 -0700

    add random photo

--- NetworkingBasicDemo/NetworkingBasicDemo/ViewController.swift
+++ NetworkingBasicDemo/NetworkingBasicDemo/ViewController.swift
@@ -2,36 +2,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let randomNum = Int(arc4random_uniform(4))
+    let URLs = ["http://imgur.com/zdwdenZ.png","http://imgur.com/bktnImE.png","http://imgur.com/CoQ8aNl.png","http://imgur.com/2vQtZBb.png","http://imgur.com/y9MIaCS.png"]
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         getImage()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    func getImage(){
-        let catPictureURL = URL(string: "http://imgur.com/zdwdenZ.png")!
+    func getImage() {
+        let catPictureURL = URL(string: URLs[randomNum])!
         let session = URLSession(configuration: .default)
-        
         let downloadPicTask = session.dataTask(with: catPictureURL) { (data, response, error) in
             // The download has finished.
             if let e = error {
                 print("Error downloading cat picture: \(e)")
             } else {
-                // No errors found.
-                // It would be weird if we didn't have a response, so check for that too.
+                // Check response.
                 if let res = response as? HTTPURLResponse {
                     print("Downloaded cat picture with response code \(res.statusCode)")
                     if let imageData = data {
-                        // Finally convert that Data into an image and do what you wish with it.
+                        // Convert the Data into an image.
                         let img = UIImage(data: imageData)
                         self.imageView.image = img
-                        // Do something with your image.
                     } else {
                         print("Couldn't get image: Image is nil")
                     }
@@ -41,8 +34,8 @@ class ViewController: UIViewController {
             }
         }
         downloadPicTask.resume()
-        
     }
 
+    
 }
 
