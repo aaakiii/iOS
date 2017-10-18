4ff915aefc53d690ddc402b5be0825721d86e7e7
aaakiii <a.okada3@gmail.com>
2017-10-18 10:35:35 -0700

    initial commit

---
+++ NetworkingBasicDemo/NetworkingBasicDemo/ViewController.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getImage()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func getImage(){
+        let catPictureURL = URL(string: "http://imgur.com/zdwdenZ.png")!
+        let session = URLSession(configuration: .default)
+        
+        let downloadPicTask = session.dataTask(with: catPictureURL) { (data, response, error) in
+            // The download has finished.
+            if let e = error {
+                print("Error downloading cat picture: \(e)")
+            } else {
+                // No errors found.
+                // It would be weird if we didn't have a response, so check for that too.
+                if let res = response as? HTTPURLResponse {
+                    print("Downloaded cat picture with response code \(res.statusCode)")
+                    if let imageData = data {
+                        // Finally convert that Data into an image and do what you wish with it.
+                        let img = UIImage(data: imageData)
+                        self.imageView.image = img
+                        // Do something with your image.
+                    } else {
+                        print("Couldn't get image: Image is nil")
+                    }
+                } else {
+                    print("Couldn't get response code for some reason")
+                }
+            }
+        }
+        downloadPicTask.resume()
+        
+    }
+
+}
+
