4374d0fa7dd85a9104b6f33727766cfea0505807
aaakiii <a.okada3@gmail.com>
2017-10-20 00:37:15 -0700

    initial commit

--- NetWorkingBasic2Demo/NetWorkingBasic2Demo/ViewController.swift
+++ NetWorkingBasic2Demo/NetWorkingBasic2Demo/ViewController.swift
@@ -1,23 +1,38 @@
-//
-//  ViewController.swift
-//  NetWorkingBasic2Demo
-//
-//  Created by 岡田暁 on 2017-10-18.
-//  Copyright © 2017 Aki. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+
+    @IBOutlet weak var tableView: UITableView!
+    let imgArray: NSArray = [
+        "img0","img1",
+        "img2","img3",
+        "img4","img5",
+        "img6","img7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imgArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell",for: indexPath)
+        return cell
+    }
+    // Set row height
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
     }
 
 
