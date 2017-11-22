ac167cc0a3a976c07b08ee5488e2f5baf72b5aba
aaakiii <a.okada3@gmail.com>
2017-11-22 11:41:17 -0800

    add assignment15

---
+++ Assignment/NewsApp/NewsTableViewCell.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet var summaryLabel: UILabel!
+    @IBOutlet var categoryLabel: UILabel!
+    var newsItem: NewsItem? {
+        didSet {
+            if let item = newsItem {
+                categoryLabel.text = item.category.toString()
+                categoryLabel.textColor = item.category.toColor()
+                summaryLabel.text = item.summary
+            }else {
+                categoryLabel.text = nil
+                summaryLabel.text = nil
+            }
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
